6006b05df0e87b53975cc32221d67c52c83252a9
khkssong6949 <bada6949@gmail.com>
2020-05-27 21:15:05 +0900

    새 액티비티 추가 및 관리자 로그인 확인 기능 추가

---
+++ app/src/main/java/kr/co/hgney/loginpractice_20200527/SignUpActivity.kt
@@ -0,0 +1,12 @@
+package kr.co.hgney.loginpractice_20200527
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SignUpActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+}
