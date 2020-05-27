1b94216ed4b5853072d2093b2cd10a838a297cee
khkssong6949 <bada6949@gmail.com>
2020-05-27 19:45:55 +0900

    로그

--- app/src/main/java/kr/co/hgney/loginpractice_20200527/MainActivity.kt
+++ app/src/main/java/kr/co/hgney/loginpractice_20200527/MainActivity.kt
@@ -2,11 +2,18 @@ package kr.co.hgney.loginpractice_20200527
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        eventBtn.setOnClickListener {
+            Log.d("이벤트확인", "버튼클릭")
+            Log.e("이벤트확인","에러로그 발생")
+        }
     }
 }
