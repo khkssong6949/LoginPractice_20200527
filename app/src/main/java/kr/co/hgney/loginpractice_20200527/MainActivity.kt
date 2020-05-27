1b97a9adc2d8f249e3eff4ed9f6968423d1dd3b2
khkssong6949 <bada6949@gmail.com>
2020-05-27 19:34:31 +0900

    Initial commit

---
+++ app/src/main/java/kr/co/hgney/loginpractice_20200527/MainActivity.kt
@@ -0,0 +1,12 @@
+package kr.co.hgney.loginpractice_20200527
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
